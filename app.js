ee53095cd70fc5e18155feb03510885ddecb7ae4
Josiah <josiah@bloomer.cc>
2024-11-05 16:23:13 -0500

    port

--- app.js
+++ app.js
@@ -354,4 +354,4 @@ function applyTurnPoints() {
 	}
 }
 
-server.listen(3000, () => console.log("Server running on port 3000"))
+server.listen(3040, () => console.log("Server running on port 3040"))
